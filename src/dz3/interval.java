eea9053c3171a9bb974df6d72d287c896e21c3ec
tatyana <tatyana.volkova1984@mail.ru>
2023-09-15 23:59:03 +0500

    домашняя работа

---
+++ src/dz3/interval.java
@@ -0,0 +1,9 @@
+package dz3;
+
+public class interval {
+    public boolean numberInInterval(int n) {
+    if (n >= 25 && n <= 100) return true;
+    else return false;
+}
+}
+
---
+++ src/dz3/parity.java
@@ -0,0 +1,10 @@
+package dz3;
+
+public class parity {public boolean evenOddNumber(int n) {
+    if (n % 2 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+}
